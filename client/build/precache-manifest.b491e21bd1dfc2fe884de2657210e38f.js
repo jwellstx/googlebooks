a60e99862c3264dee8da36e303a466f970bf0fc3
JWeez <jwellstx@gmail.com>
2020-06-15 21:19:04 -0500

    pushing to heroku

---
+++ client/build/precache-manifest.b491e21bd1dfc2fe884de2657210e38f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21277b660d992df3d70",
+    "url": "/static/css/main.344a9e53.chunk.css"
+  },
+  {
+    "revision": "a21277b660d992df3d70",
+    "url": "/static/js/main.9ba369ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66ce17eb68b43d65012a",
+    "url": "/static/js/2.694c8f6a.chunk.js"
+  },
+  {
+    "revision": "1e0b1fed60ef09c1a16d6ace7faa6baa",
+    "url": "/index.html"
+  }
+];
